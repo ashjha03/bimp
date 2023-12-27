970e4a1a06f7c59efcacf8f98ef1a8e59df7ed60
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-12-27 22:07:10 +0530

    Profile Box Created

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -25,16 +25,15 @@ const Featured = ({ title, description }) => {
         <h1 className="text-3xl font-bold text-pink-600">{title}</h1>
         <p className="text-lg font-serif pl-2">{description}</p>
       </div>
-      {users.length ? (
-        users.map((user) => {
-          <div key={user._id} className="profiles grid grid-cols-4 gap-4 py-4">
-            {/* <ProfileBox user={user} /> */}
-            <h1>hello</h1>
-          </div>;
-        })
-      ) : (
-        <h1>Loading ...</h1>
-      )}
+      <div className="profiles grid grid-cols-4 gap-4 py-4">
+        {users.length ? (
+          users.map((user) => {
+            return <ProfileBox key={user._id} user={user} />;
+          })
+        ) : (
+          <h1>Loading ...</h1>
+        )}
+      </div>
     </div>
   );
 };
